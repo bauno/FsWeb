1b7d5fff8e7ad02483a9cb6fc49ed5416f7c5fcf
Alessandro Bonometti <alessandro.bonometti@studiofarma.it>
2016-06-23 16:28:39 +0200

    Cache in Repository

--- FsWeb/FsWebWebApp/Repository.fs
+++ FsWeb/FsWebWebApp/Repository.fs
@@ -1,8 +1,26 @@
-﻿module Repository
+﻿namespace FsWeb.Repositories
+
+module Repository =
     open System.Linq
+    open CacheAgent
+
+    let withCacheKeyOf key = Some key
+
+    let doNotUseCache = None
     
-    let get (source: IQueryable<_>) queryFn =
-        queryFn source |> Seq.toList
+    let get (source: IQueryable<_>) queryFn  cacheKey =
+        let cacheResult =
+            match cacheKey with
+            | Some key -> CacheAgent.get<'a list> key
+            | None -> None
+        match cacheResult, cacheKey with
+        | Some result, _ -> result
+        | None, Some cacheKey ->
+            let result = queryFn source |> Seq.toList
+            CacheAgent.set cacheKey cacheResult
+            result
+        | _, _ -> queryFn source |> Seq.toList
+        
 
     let getAll() =
         fun s -> query {for x in s do select x}
